d5e01ca844722b89202c748949ba98cd41442211
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2015-02-02 22:46:32 -0200

    Migrate tests

--- lib/fog/riakcs.rb
+++ lib/fog/riakcs.rb
@@ -1,11 +1,12 @@
 require "fog/riakcs/version"
+require "fog/core"
 
 module Fog
   module RiakCS
     extend Fog::Provider
 
     autoload :MultipartUtils, 'fog/riakcs/multipart_utils'
-    autoload :Provisionin, 'fog/riakcs/provisioning'
+    autoload :Provisioning, 'fog/riakcs/provisioning'
     autoload :Utils, 'fog/riakcs/utils'
     autoload :UserUtils, 'fog/riakcs/user_utils'
     autoload :Usage, 'fog/riakcs/usage'
